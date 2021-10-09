b1b7adb449e02d665e6da6d4e8d14d5a8c7e2937
Věnčeslav Chalupář <90437977+MetrKolben@users.noreply.github.com>
2021-10-09 21:08:21 +0200

    0.2

--- code.js
+++ code.js
@@ -16,9 +16,20 @@ function repaint() {
     //var img = new Image();
     //img.src = './brain.png'
     //ctx.drawImage(img, 100, 100, 100, 100);
-    let organ = notInserted[0];
-    //console.log(organ.organ);
-    ctx.drawImage(new Image(organ.src), organ.x, organ.y, organ.width, organ.height);
+    //var img2 = new Image();
+    //let organ = notInserted[0];
+    //console.log(organ.organ.src);
+    //var img = new Image();
+    //img.src = organ.organ.src;
+    //ctx.drawImage(img, 100, 100, 100, 100);
+    //ctx.drawImage(new Image(organ.organ.src), organ.x, organ.y, organ.width, organ.height);
+    for(organ of notInserted) {
+        var img = new Image();
+        img.src = organ.organ.src;
+        var o = organ.organ;
+        ctx.drawImage(img, o.x, o.y, o.width, o.height);
+    }
+    
 }
 
 function main() {
@@ -27,16 +38,24 @@ function main() {
     //img.src = './brain.png'
     //ctx.drawImage(img, 100, 100);
     setInterval(repaint, 1000/60);
+    console.log(window.outerWidth);
 }
 
-const defWidth = 650;
-const defHeight = 800;
-const scale = window.innerWidth / 1920;
+const defWidth = 1300;
+const defHeight = 1600;
+const scale = window.outerWidth / 1920 * 2;
 const cWidth = defWidth * scale;
 const cHeight = defHeight * scale;
 var inserted = new Array();
 var notInserted = [
-    {organ: new Organ("./brain.png", 100, 100, 100, 100, 100, 100, 2)}
+    {organ: new Organ("./imgs/intestines.png", 700*scale, 20*scale, 200*scale, 240*scale, 280*scale, 620*scale, 1)},
+    {organ: new Organ("./imgs/bladder.png", 315*scale, 700*scale, 130*scale, 160*scale, 315*scale, 700*scale, 2)},
+    {organ: new Organ("./imgs/stomach.png", 330*scale, 500*scale, 170*scale, 160*scale, 330*scale, 500*scale, 3)},
+    {organ: new Organ("./imgs/kidneys.png", 280*scale, 560*scale, 200*scale, 120*scale, 280*scale, 560*scale, 4)},
+    {organ: new Organ("./imgs/liver.png", 260*scale, 500*scale, 190*scale, 130*scale, 260*scale, 500*scale, 5)},
+    {organ: new Organ("./imgs/lungs.png", 240*scale, 280*scale, 280*scale, 240*scale, 240*scale, 280*scale, 6)},
+    {organ: new Organ("./imgs/heart.png", 325*scale, 300*scale, 105*scale, 140*scale, 325*scale, 300*scale, 7)},
+    {organ: new Organ("./imgs/brain.png", 320*scale, 10*scale, 120*scale, 90*scale, 320*scale, 10*scale, 8)}
 ];
 
 
